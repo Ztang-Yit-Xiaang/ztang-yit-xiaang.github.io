e4c0d6bfd3be4de4bc4bfc7cbcbbf3ec102c06a2
Yit Xiaang Ztang <ztangyitxiaang@gmail.com>
2026-03-15 23:17:26 -0500

    update photo layout

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -6,24 +6,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
     EXIF.getData(img, function(){
 
-      const camera = EXIF.getTag(this, "Model");
-      const lens = EXIF.getTag(this, "LensModel");
-      const iso = EXIF.getTag(this, "ISOSpeedRatings");
-      const aperture = EXIF.getTag(this, "FNumber");
-      const shutter = EXIF.getTag(this, "ExposureTime");
+      const lat = EXIF.getTag(this, "GPSLatitude");
+      const lon = EXIF.getTag(this, "GPSLongitude");
+      const latRef = EXIF.getTag(this, "GPSLatitudeRef");
+      const lonRef = EXIF.getTag(this, "GPSLongitudeRef");
 
-      let exifHTML = "";
+      function convertDMSToDD(dms, ref) {
+        let dd = dms[0] + dms[1]/60 + dms[2]/3600;
+        if (ref === "S" || ref === "W") dd = dd * -1;
+        return dd;
+      }
 
-      if(camera) exifHTML += `📷 ${camera} `;
-      if(lens) exifHTML += `🔍 ${lens} `;
-      if(iso) exifHTML += `ISO ${iso} `;
-      if(aperture) exifHTML += `f/${aperture.numerator/aperture.denominator} `;
-      if(shutter) exifHTML += `⏱ ${shutter.numerator}/${shutter.denominator}s`;
+      if(lat && lon){
 
-      const container = img.closest(".photo-card").querySelector(".photo-exif");
+        const latitude = convertDMSToDD(lat, latRef);
+        const longitude = convertDMSToDD(lon, lonRef);
 
-      if(container){
-        container.innerHTML = exifHTML;
+        const mapContainer = img.closest(".photo-card").querySelector(".photo-map");
+
+        if(mapContainer){
+
+          const map = L.map(mapContainer).setView([latitude, longitude], 10);
+
+          L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution: "© OpenStreetMap"
+          }).addTo(map);
+
+          L.marker([latitude, longitude]).addTo(map);
+
+        }
+
       }
 
     });
